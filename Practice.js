34b7744559daedb506c1482b4561c15f4840097e
Ben Khazaal <thajukez101@gmail.com>
2024-01-24 15:06:05 -0700

    button-fixed

--- Practice.js
+++ Practice.js
@@ -1,9 +1,7 @@
 function TheLink() {
   var yuhButton = document.getElementById("yuh");
-  n;
   yuhButton.addEventListener("click", function () {
     window.location.href = "https://www.nintendo.com/us/";
-    console.log("the button works");
   });
 }
 
